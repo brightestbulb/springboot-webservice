7d023c9188d59292dc4dabbf167b31a4b1894af7
brightestbulb <gisun9021@naver.com>
2020-01-26 17:45:27 +0900

    User.java, Role.java

---
+++ src/main/java/com/brightest/study/springboot/domain/user/User.java
@@ -0,0 +1,52 @@
+package com.brightest.study.springboot.domain.user;
+
+import com.brightest.study.springboot.domain.posts.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User  extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)  // JPA로 DB 저장할 때 Enum값을 어떤 형태로 저장할지 결정. default는 int
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role){
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture){
+        this.name = name;
+        this.picture = picture;
+        return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+
+
+
+}
